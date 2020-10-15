6f2e1d3039ba855fd53f86c75742bf85c65b07d2
Paula Deville <paula.deville@posteo.net>
2020-10-15 18:27:09 +0200

    Refactoring

--- script.js
+++ script.js
@@ -1,27 +1,18 @@
 const accordions = document.querySelectorAll(".accordion");
 
 for (let accordion of accordions) {
-    accordion.addEventListener("click", function() {
-        if(document.getElementsByClassName("displayed-answer")) {
-            this.classList.remove("displayed-answer");
-        }
-        accordion.querySelector(".answer").classList.toggle("displayed-answer");
-        accordion.querySelector(".question").classList.toggle("selected-question");
-        accordion.querySelector(".arrow").classList.toggle("upside-arrow");
+    accordion.addEventListener("click", () => {
+        accordion.querySelector(".answer").classList.toggle("answer--active");
+        accordion.querySelector(".question").classList.toggle("question--active");
+        accordion.querySelector(".arrow").classList.toggle("arrow--active");
     })
-}
+};
 
 for (let accordion of accordions) {
-    accordion.addEventListener("mouseover", function() {
-        document.querySelector("#img-box-desktop").classList.add("translated-box");
+    accordion.addEventListener("mouseover", () => {
+        document.querySelector(".img-box-desktop").classList.add("img-box-desktop--active");
     })
-}
-
-for (let accordion of accordions) {
-    accordion.addEventListener("mouseout", function() {
-        document.querySelector("#img-box-desktop").classList.remove("translated-box");
+    accordion.addEventListener("mouseout", () => {
+        document.querySelector(".img-box-desktop").classList.remove("img-box-desktop--active");     
     })
-}
-
-
-
+};
